0767fbcdcfae68b47669b79138d97edee8ff8beb
ShengJie <shengjie0202@outlook.com>
2020-07-09 09:59:40 +0800

    move f data to single file

--- src/pano/pano.ts
+++ src/pano/pano.ts
@@ -5,7 +5,9 @@ import { drawPoints } from 'src/pano/drawPoints';
 import { JRect, JPoint } from 'src/math';
 import { JPainter } from 'src/painter';
 import { JProgram } from 'src/webgl';
-import { fUrl } from 'src/pano/assets/f_png';
+
+import { fVertexData } from 'src/pano/assets/f_vertex';
+import { fColorData } from 'src/pano/assets/f_color';
 
 const setting = {
 	x: 111,
@@ -27,10 +29,6 @@ function main() {
 	if (!gl) {
 		return;
 	}
-
-	const img = new Image();
-	img.src = fUrl;
-	img.onload = () => console.log('loaded')
 
 	createSetting(() => {
 		render(gl);
@@ -141,13 +139,12 @@ function drawF(gl: WebGLRenderingContext) {
 	// create vertex buffer
 	const buffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-	const data = new Float32Array(vertexData);
-	gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(fVertexData), gl.STATIC_DRAW);
 
 	// create color buffer
 	const colorBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Uint8Array(colorData), gl.STATIC_DRAW);
+	gl.bufferData(gl.ARRAY_BUFFER, fColorData, gl.STATIC_DRAW);
 
 	// asign to attribute
 	const location = gl.getAttribLocation(program.program, 'a_position');
@@ -155,10 +152,10 @@ function drawF(gl: WebGLRenderingContext) {
 	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 	gl.vertexAttribPointer(location, 3, gl.FLOAT, false, 0, 0);
 
-	// gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-	// const colorLocation = gl.getAttribLocation(program.program, 'a_color');
-	// gl.enableVertexAttribArray(colorLocation);
-	// gl.vertexAttribPointer(colorLocation, 3, gl.UNSIGNED_BYTE, true, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+	const colorLocation = gl.getAttribLocation(program.program, 'a_color');
+	gl.enableVertexAttribArray(colorLocation);
+	gl.vertexAttribPointer(colorLocation, 3, gl.UNSIGNED_BYTE, true, 0, 0);
 
 	gl.drawArrays(gl.TRIANGLES, 0, 16 * 6);
 }
@@ -280,263 +277,3 @@ function createSlider(name: string, value: number, minValue: number, maxValue: n
 
 	return wrapper;
 }
-
-const vertexData = [
-	// left column front
-	0, 0, 0,
-	0, 150, 0,
-	30, 0, 0,
-	0, 150, 0,
-	30, 150, 0,
-	30, 0, 0,
-
-	// top rung front
-	30, 0, 0,
-	30, 30, 0,
-	100, 0, 0,
-	30, 30, 0,
-	100, 30, 0,
-	100, 0, 0,
-
-	// middle rung front
-	30, 60, 0,
-	30, 90, 0,
-	67, 60, 0,
-	30, 90, 0,
-	67, 90, 0,
-	67, 60, 0,
-
-	// left column back
-	0, 0, 30,
-	30, 0, 30,
-	0, 150, 30,
-	0, 150, 30,
-	30, 0, 30,
-	30, 150, 30,
-
-	// top rung back
-	30, 0, 30,
-	100, 0, 30,
-	30, 30, 30,
-	30, 30, 30,
-	100, 0, 30,
-	100, 30, 30,
-
-	// middle rung back
-	30, 60, 30,
-	67, 60, 30,
-	30, 90, 30,
-	30, 90, 30,
-	67, 60, 30,
-	67, 90, 30,
-
-	// top
-	0, 0, 0,
-	100, 0, 0,
-	100, 0, 30,
-	0, 0, 0,
-	100, 0, 30,
-	0, 0, 30,
-
-	// top rung right
-	100, 0, 0,
-	100, 30, 0,
-	100, 30, 30,
-	100, 0, 0,
-	100, 30, 30,
-	100, 0, 30,
-
-	// under top rung
-	30, 30, 0,
-	30, 30, 30,
-	100, 30, 30,
-	30, 30, 0,
-	100, 30, 30,
-	100, 30, 0,
-
-	// between top rung and middle
-	30, 30, 0,
-	30, 60, 30,
-	30, 30, 30,
-	30, 30, 0,
-	30, 60, 0,
-	30, 60, 30,
-
-	// top of middle rung
-	30, 60, 0,
-	67, 60, 30,
-	30, 60, 30,
-	30, 60, 0,
-	67, 60, 0,
-	67, 60, 30,
-
-	// right of middle rung
-	67, 60, 0,
-	67, 90, 30,
-	67, 60, 30,
-	67, 60, 0,
-	67, 90, 0,
-	67, 90, 30,
-
-	// bottom of middle rung.
-	30, 90, 0,
-	30, 90, 30,
-	67, 90, 30,
-	30, 90, 0,
-	67, 90, 30,
-	67, 90, 0,
-
-	// right of bottom
-	30, 90, 0,
-	30, 150, 30,
-	30, 90, 30,
-	30, 90, 0,
-	30, 150, 0,
-	30, 150, 30,
-
-	// bottom
-	0, 150, 0,
-	0, 150, 30,
-	30, 150, 30,
-	0, 150, 0,
-	30, 150, 30,
-	30, 150, 0,
-
-	// left side
-	0, 0, 0,
-	0, 0, 30,
-	0, 150, 30,
-	0, 0, 0,
-	0, 150, 30,
-	0, 150, 0,
-]
-
-const colorData = [
-	// left column front
-	200, 70, 120,
-	200, 70, 120,
-	200, 70, 120,
-	200, 70, 120,
-	200, 70, 120,
-	200, 70, 120,
-
-	// top rung front
-	200, 70, 120,
-	200, 70, 120,
-	200, 70, 120,
-	200, 70, 120,
-	200, 70, 120,
-	200, 70, 120,
-
-	// middle rung front
-	200, 70, 120,
-	200, 70, 120,
-	200, 70, 120,
-	200, 70, 120,
-	200, 70, 120,
-	200, 70, 120,
-
-	// left column back
-	80, 70, 200,
-	80, 70, 200,
-	80, 70, 200,
-	80, 70, 200,
-	80, 70, 200,
-	80, 70, 200,
-
-	// top rung back
-	80, 70, 200,
-	80, 70, 200,
-	80, 70, 200,
-	80, 70, 200,
-	80, 70, 200,
-	80, 70, 200,
-
-	// middle rung back
-	80, 70, 200,
-	80, 70, 200,
-	80, 70, 200,
-	80, 70, 200,
-	80, 70, 200,
-	80, 70, 200,
-
-	// top
-	70, 200, 210,
-	70, 200, 210,
-	70, 200, 210,
-	70, 200, 210,
-	70, 200, 210,
-	70, 200, 210,
-
-	// top rung right
-	200, 200, 70,
-	200, 200, 70,
-	200, 200, 70,
-	200, 200, 70,
-	200, 200, 70,
-	200, 200, 70,
-
-	// under top rung
-	210, 100, 70,
-	210, 100, 70,
-	210, 100, 70,
-	210, 100, 70,
-	210, 100, 70,
-	210, 100, 70,
-
-	// between top rung and middle
-	210, 160, 70,
-	210, 160, 70,
-	210, 160, 70,
-	210, 160, 70,
-	210, 160, 70,
-	210, 160, 70,
-
-	// top of middle rung
-	70, 180, 210,
-	70, 180, 210,
-	70, 180, 210,
-	70, 180, 210,
-	70, 180, 210,
-	70, 180, 210,
-
-	// right of middle rung
-	100, 70, 210,
-	100, 70, 210,
-	100, 70, 210,
-	100, 70, 210,
-	100, 70, 210,
-	100, 70, 210,
-
-	// bottom of middle rung.
-	76, 210, 100,
-	76, 210, 100,
-	76, 210, 100,
-	76, 210, 100,
-	76, 210, 100,
-	76, 210, 100,
-
-	// right of bottom
-	140, 210, 80,
-	140, 210, 80,
-	140, 210, 80,
-	140, 210, 80,
-	140, 210, 80,
-	140, 210, 80,
-
-	// bottom
-	90, 130, 110,
-	90, 130, 110,
-	90, 130, 110,
-	90, 130, 110,
-	90, 130, 110,
-	90, 130, 110,
-
-	// left side
-	160, 160, 220,
-	160, 160, 220,
-	160, 160, 220,
-	160, 160, 220,
-	160, 160, 220,
-	160, 160, 220,
-]
