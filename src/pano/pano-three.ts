51325118498e9770732b3a47f187479d7467dbab
ShengJie <shengjie0202@outlook.com>
2020-07-28 10:26:27 +0800

    修复了全景图镜像的问题

--- src/pano/pano-three.ts
+++ src/pano/pano-three.ts
@@ -14,6 +14,8 @@ export function runPano() {
 		1000,
 	)
 
+	camera.applyQuaternion(new THREE.Quaternion(-0.108813, -0.843762, -0.186910, 0.4912108));
+
 	const renderer = new THREE.WebGLRenderer();
 	document.body.append(renderer.domElement);
 
@@ -24,6 +26,8 @@ export function runPano() {
 	bindCamera(camera, document as any);
 
 	scene.add(pano);
+	const model = createModel();
+	scene.add(model);
 
 	function render() {
 		renderer.setSize(window.innerWidth, window.innerHeight);
@@ -37,23 +41,39 @@ export function runPano() {
 	render();
 }
 
-function createPano(onLoad: () => void) {
+function createModel() {
 	const size = 5;
+	const geo = new THREE.BoxGeometry(size, size, size);
+	const mat = new THREE.MeshBasicMaterial({
+		color: 'red',
+		depthTest: false,
+	})
+	const mesh = new THREE.Mesh(geo, mat);
+	const distance = 50;
+	mesh.position.set(distance, 0, distance);
+
+	return mesh;
+}
+
+function createPano(onLoad: () => void) {
+	const size = 50;
 	const geo = new THREE.BoxGeometry(
 		size,
 		size,
 		size,
 	);
 
-	const mats = pano1.map(url => {
-		const loader = new THREE.TextureLoader();
-		const texture = loader.load(url);
-		return new THREE.MeshBasicMaterial({
-			map: texture,
-			side: THREE.BackSide,
-		})
-	})
-	const mesh = new THREE.Mesh(geo, mats);
+	// const mats = pano1.map(url => {
+	// 	const loader = new THREE.TextureLoader();
+	// 	const texture = loader.load(url);
+	// 	return new THREE.MeshBasicMaterial({
+	// 		map: texture,
+	// 		side: THREE.BackSide,
+	// 	})
+	// })
+
+	const mat = createPanoMaterial();
+	const mesh = new THREE.Mesh(geo, mat);
 
 	return mesh;
 }
@@ -82,4 +102,37 @@ function bindCamera(camera: THREE.PerspectiveCamera, el: HTMLElement) {
 		x = ev.x;
 		y = ev.y;
 	})
+}
+
+function createPanoMaterial() {
+	const vert = `
+	varying vec3 v_direction;
+	void main() {
+		gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+		v_direction = vec3(-position.x, position.yz);
+	}
+	`;
+
+	const frag = `
+	uniform samplerCube panoCubeMap;
+	varying vec3 v_direction;
+
+	void main() {
+		gl_FragColor = textureCube(panoCubeMap, v_direction);
+	}
+	`;
+
+	const texture = new THREE.CubeTextureLoader().load(pano1);
+	const mat = new THREE.ShaderMaterial({
+		vertexShader: vert,
+		fragmentShader: frag,
+		uniforms: {
+			panoCubeMap: {
+				value: texture,
+			}
+		},
+		side: THREE.BackSide,
+	})
+
+	return mat;
 }
